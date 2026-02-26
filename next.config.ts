c943bdec8b0bd0542f6d6dfc040381254a189509
tayyabjamshaid <tayyab.devblends@gmail.com>
2026-02-26 15:16:51 +0500

    feat : pwa implemented

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import withPWA from 'next-pwa';
+import type { NextConfig } from 'next';
+
+const isDev = process.env.NODE_ENV !== 'production';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+const pwa = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: isDev,
+});
+
+export default pwa(nextConfig as any); // ✅ FIX
